a8fdcbe7d73aca022093890a54e20089c364d43f
Bryan-Gracida-Tapia <li01231009@unsij.edu.mx>
2026-05-05 09:56:56 -0600

    Creación de limitaciones

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,6 +8,10 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 // Test conexión
